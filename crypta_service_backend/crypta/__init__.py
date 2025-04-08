a6f2ed02e5dbe229d88a7bcf69a22b9b4b76ba84
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-04-08 14:38:20 -0400

    Added CORS, restframework, and new app into the settings as well as the needed settings.

---
+++ crypta_service_backend/crypta/__init__.py
@@ -0,0 +1,13 @@
+"""
+Used for API Backend management of the Crypta service.
+This module contains the Django application configuration for the Crypta service.
+It sets up the necessary configurations for the Django application, including
+database settings, authentication backends, and middleware.
+It also includes the URL routing for the API and the admin interface.
+It is important to note that this module is not intended to be run directly.
+Instead, it should be imported and used by the Django application.
+It is recommended to use a virtual environment to manage dependencies and
+isolate the application from the system Python installation.
+It is also recommended to use a configuration management tool to manage
+the settings for different environments (e.g., development, testing, production).
+"""
--- crypta_service_backend/crypta_service_backend/settings.py
+++ crypta_service_backend/crypta_service_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,11 +30,28 @@ SECRET_KEY = 'django-insecure-ymygc$$g!1#vv(p6(#gta@=cpg_d(l_-wy))gdgpdmb$$fy4i9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
+# Needed for JWT Tokens
+# https://www.django-rest-framework.org/api-guide/authentication/#json-web-token-authentication
+# https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
 
 # Application definition
 
+#Added crypta app, rest_framework and corsheaders
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +59,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crypta",
+    'rest_framework',
+    'corsheaders' 
 ]
 
 MIDDLEWARE = [
@@ -47,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'crypta_service_backend.urls'
@@ -120,3 +146,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS settings
+# https://pypi.org/project/django-cors-headers/
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True

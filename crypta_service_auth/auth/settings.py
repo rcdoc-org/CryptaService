6cb862fa7a30e89c7133a7c6fb9c0647536752c2
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-04-09 13:11:26 -0400

    Added the required configs for mySQL using environment variables.

--- crypta_service_auth/auth/settings.py
+++ crypta_service_auth/auth/settings.py
@@ -99,10 +99,20 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        "USER": os.environ.get('MYSQL_USER'),
+        "PASSWORD": os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
 }
 
 

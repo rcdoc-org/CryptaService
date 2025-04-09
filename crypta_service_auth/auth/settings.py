4a370ae2a441263b91b82a87123f0d8f5468fac4
Kyle Greenberg <kyle.greenberg93@gmail.com>
2025-04-09 14:02:49 -0400

    Added the needed items to move forward with mySQL for users.
    This project will likely move to a noSQL for users in the future but for
    point of a demo we need a secure method of hosting for our users to test,
    during development.

--- crypta_service_auth/auth/settings.py
+++ crypta_service_auth/auth/settings.py
@@ -25,12 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ymygc$$g!1#vv(p6(#gta@=cpg_d(l_-wy))gdgpdmb$$fy4i9'
+# SECRET_KEY = 'django-insecure-ymygc$$g!1#vv(p6(#gta@=cpg_d(l_-wy))gdgpdmb$$fy4i9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', True))
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 # Needed for JWT Tokens
 # https://www.django-rest-framework.org/api-guide/authentication/#json-web-token-authentication
